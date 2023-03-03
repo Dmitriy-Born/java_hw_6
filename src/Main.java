3000ff158475633734a1a57561d36a748d62f29b
Dmitriy-Born <1111born1111@gmail.com>
2023-03-03 23:23:10 +0300

    Commit2

--- src/Main.java
+++ src/Main.java
@@ -23,9 +23,9 @@ class TreeNode {
     public static List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root != null) {
-            result.add(root.val); // посещаем корень
-            result.addAll(preorderTraversal(root.left)); // обходим левое поддерево
-            result.addAll(preorderTraversal(root.right)); // обходим правое поддерево
+            result.add(root.val);
+            result.addAll(preorderTraversal(root.left));
+            result.addAll(preorderTraversal(root.right));
         }
         return result;
     }
